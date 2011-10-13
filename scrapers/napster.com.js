884b4acb38922460f596b8d2619543350ec0fa14
Jason Herskowitz <jherskowitz@globallistic.com>
2011-10-13 18:40:43 -0400

    Make Napster scraper work

--- scrapers/napster.com.js
+++ scrapers/napster.com.js
@@ -12,12 +12,11 @@ Playgrub.source.url = 'http://.*\.napster.com/playlist.htm.*';
 Playgrub.source.error = 'Sorry, no tracks were found.'
 Playgrub.source.scrape = function() {
 
-    $("td.trackInfo").each(function() {
-        var artist = $(this).find('a:last-child').text();
-        var song = $(this).find('a.').text();
-
-        artist = $.trim(artist);
-        song = $.trim(song);
+   $("td.trackInfo").each(function() {
+       var artist = $.trim($(this).find('span.artistName a').text());
+       var song = $.trim($(this).find('a.playNoButton').text());
+       artist = $.trim(artist);
+       song = $.trim(song);
 
         if (artist && song) {
             Playgrub.playlist.add_track(artist, song);
