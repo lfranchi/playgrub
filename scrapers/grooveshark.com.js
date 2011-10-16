f3c4cc1fe344ff3df31ac1d37800c2d452ffc1fc
Jason Herskowitz <jherskowitz@globallistic.com>
2011-10-16 17:22:34 -0400

    Another try at Grooveshark scrapper

--- scrapers/grooveshark.com.js
+++ scrapers/grooveshark.com.js
@@ -1,18 +1,19 @@
 /**
  * Grooveshark Playgrub.source
- * Created by: Toby Padilla <tobypadilla AT gmail DOT com>
+ * Created by: jherskowitz <jherskowitz AT globallistic DOT com>
  * Version: 0.1
  *
  * Notes:
  *
- * This scraper will work on any Last.fm song page and only add unique songs.
+ * This scraper will work on any Grooveshark playlist.
  */
 
-Playgrub.source.url = 'http://widgets\.grooveshark\.com/add_songs.*';
-Playgrub.source.error = "To use Tomahawklet on Grooveshark export a song list, choose \"Mirror Changes\" or \"Copy Songs\", scroll down and hit \"Next Step\", then use the bookmarklet.";
+Playgrub.source.url = 'http://grooveshark\.com/\#/playlist/\.*';
+Playgrub.source.error = "Check your Grooveshark page, Tomahawklet only works on playlists.";
 Playgrub.source.scrape = function() {
-    $("h4").each(function () {
-        var song_result = $(this).html().split(" - ");
+ $("div.slick-cell").each(function() {
+        var song = $.trim($(this).find('a.songLink').text());
+        var artist = $.trim($(this).find('a.field').text());
         Playgrub.playlist.add_track(song_result[1], song_result[0]);
     });
 }
