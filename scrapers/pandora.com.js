878fa1272376a8c468433e230c5f795b5e6b3a3a
Jason Herskowitz <jherskowitz@globallistic.com>
2011-10-26 16:07:52 -0400

    First try at Pandora song pages

---
+++ scrapers/pandora.com.js
@@ -0,0 +1,27 @@
+
+/**
+ * Pandora Song Pages Tomahawklet Scraper
+ * Created by: Jason Herskowitz <jherskowitz AT globallistic DOT com>
+ * Version: 0.1
+ *
+ * Notes:
+ *
+ * This scraper will work on Pandora song pages.
+ */
+Playgrub.source.url = 'http://\www.pandora.com\/\#\/\music/\song.*';
+Playgrub.source.error = 'Sorry, no tracks were found.'
+Playgrub.source.scrape = function() {
+
+    $("div.column-right").each(function() {
+        var song = $.trim($(this).find('h2').text());
+        var artist = $.trim($(this).find('h3').text());
+        console.log(song + ' - ' + artist);
+});
+
+	
+
+       Playgrub.playlist.add_track(artist, song);
+    });
+}
+
+Playgrub.source.start();
