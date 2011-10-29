a56099ceaa185504a41bf37fd571ea9398234168
Jason Herskowitz <jherskowitz@globallistic.com>
2011-10-28 21:33:18 -0400

    First try at NPR World Cafe scraper

--- scrapers/vevo.com.js
+++ scrapers/vevo.com.js
@@ -13,11 +13,10 @@ Playgrub.source.error = 'Sorry, no tracks were found.'
 
 Playgrub.source.scrape = function() {
 
-    $('div.playlist-title').each(function() {
-        var artist = $(this).find('div.channel-playlist-area > a:first-child').text();
-        var song = ($(this).find('div.video-info').text();
-
-     
+   $("div.playlist-title").each(function() {
+        var song = $(this).find('div.video-info span').text();
+        var artist = $(this).find('div.channel-playlist-area a').text();
+             
         if (artist && song) {
             Playgrub.playlist.add_track(artist, song);
         
